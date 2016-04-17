e6e4223bc6b92148150c7d767419ead91cf6ecbb
root <root@nrtservices.eastus.cloudapp.azure.com>
2016-04-17 21:17:32 +0800

    Refactor backend model and document

--- README.md
+++ README.md
@@ -15,12 +15,17 @@ python api.py
 ```
 
 ## Test
-```
-$ curl http://localhost:5000/backends
-{"espeak": {"info": "http://espeak.sourceforge.net/", "binary": "/usr/bin/espeak", "parameters": {"phoneme": "-x", "ssml": "-m", "version": {"p": "--version"}, "help": {"p": "--help"}, "text": {"v": ""}, "text file": {"p": "-f", "v": ""}, "output": {"p": "-w", "v": ""}, "voice": {"p": "-v", "v": ""}, "quiet": "-q"}}, "flite": {"info": "http://www.festvox.org/flite/", "binary": "./flite2/flite", "parameters": {"phoneme": "-ps", "ssml": "-ssml", "version": {"p": "--version"}, "help": {"p": "--help"}, "text": {"p": "-t", "v": ""}, "text file": {"p": "-f", "v": ""}, "output": {"p": "-o", "v": ""}, "voice": {"p": "-voice", "v": ""}, "quiet": "-o /dev/null"}}}
+Open http://localhost:5000/ to view the API document and try it out.
 
-$ curl http://localhost:5000/backends/espeak -d "text=say something" -X POST
-{"phonemes": " s'eI s'VmTIN\n", "cmd": ["/usr/bin/espeak", "-x", "-q", "say something"]}
+```
+$ curl -X GET --header 'Accept: application/json' 'http://localhost:5000/speaks/'
+[{"backend": {"info": "http://espeak.sourceforge.net/", "parameters": [{"name": "text", "type": 0, "required": true, "arg": " "}, {"name": "phoneme", "type": -1, "required": false, "arg": "-x"}, {"name": "quiet", "type": -1, "required": false, "arg": "-q"}], "binary": "/usr/bin/espeak"}, "name": "espeak"}, {"backend": {"info": "http://www.festvox.org/flite/", "parameters": [{"name": "text", "type": -1, "required": true, "arg": "-t"}, {"name": "phoneme", "type": -1, "required": false, "arg": "-ps"}, {"name": "quiet", "type": -1, "required": false, "arg": "-o /dev/null"}], "binary": "/usr/bin/flite"}, "name": "flite"}]
+
+$ curl -X GET --header 'Accept: application/json' 'http://localhost:5000/speaks/espeak'
+{"info": "http://espeak.sourceforge.net/", "parameters": [{"required": true, "type": 0, "name": "text", "arg": " "}, {"required": false, "type": -1, "name": "phoneme", "arg": "-x"}, {"required": false, "type": -1, "name": "quiet", "arg": "-q"}], "binary": "/usr/bin/espeak"}
+
+$ curl -X POST --header 'Content-Type: application/x-www-form-urlencoded' --header 'Accept: application/json' -d 'text=say%20something' 'http://localhost:5000/speaks/flite'
+{"value": "pau s ey s aa m th ih ng pau \n", "cmd": "['/usr/bin/flite', '-ps', '-o /dev/null', '-t', 'say something']"}
 ```
 
 ## TODO
--- api.py
+++ api.py
@@ -1,105 +1,117 @@
 # This Python file uses the following encoding: utf-8
 from flask import Flask
-from flask_restplus import Api, Resource, reqparse, abort
+from flask_restplus import Api, Resource, fields
 from subprocess import check_output
 
 app = Flask(__name__)
-api = Api(app)
+api = Api(app, version='0.1', title='Speak API',
+          description='Get phonemes of a text from various TTS backends')
+
+ns = api.namespace('speaks', descripton='Speak operations')
 
 # TODO: Backends should be initialized pragmatically.
 Backends = {
     'espeak': {
         'binary': '/usr/bin/espeak',
         'info': 'http://espeak.sourceforge.net/',
-        'parameters': {
-            'version': {'p': '--version'},
-            'help': {'p': '--help'},
-            'output': {'p': '-w', 'v': ''},
-            'text file': {'p': '-f', 'v': ''},
-            'text': {'v': ''},
-            'voice': {'p': '-v', 'v': ''},
-            'ssml': '-m',
-            'phoneme': '-x',
-            'quiet': '-q',
-        },
+        'parameters': [
+            {'name': 'text', 'arg': ' ', 'type': 0, 'required': True},
+            {'name': 'phoneme', 'arg': '-x'},
+            {'name': 'quiet', 'arg': '-q'},
+        ],
     },
     'flite': {
-        'binary': './flite2/flite',
+        'binary': '/usr/bin/flite',
         'info': 'http://www.festvox.org/flite/',
-        'parameters': {
-            'version': {'p': '--version'},
-            'help': {'p': '--help'},
-            'output': {'p': '-o', 'v': ''},
-            'text file': {'p': '-f', 'v': ''},
-            'text': {'p': '-t', 'v': ''},
-            'voice': {'p': '-voice', 'v': ''},
-            'ssml': '-ssml',
-            'phoneme': '-ps',
-            'quiet': '-o /dev/null',
-        },
+        'parameters': [
+            {'name': 'text', 'arg': '-t', 'required': True},
+            {'name': 'phoneme', 'arg': '-ps'},
+            {'name': 'quiet', 'arg': '-o /dev/null'},
+        ],
     },
 }
 
+parameter = api.model('Parameter', {
+    'name': fields.String(required=True, description='The option name'),
+    'arg': fields.String(required=True, description='The argument/switch'),
+    'type': fields.Integer(default=-1, description='A custom field tba'),
+    'required': fields.Boolean(default=False),
+})
 
-def abort_if_backend_isnt_available(backend_name):
-    if backend_name not in Backends:
-        abort(404, message="Backend {} isn't available.".format(backend_name))
+backend = api.model('Backend', {
+    'binary': fields.String(required=True, description='The backend binary'),
+    'info': fields.String(required=True, description='The info site'),
+    'parameters': fields.List(fields.Nested(parameter, required=True,
+                                            description='The parameter list')),
+})
+
+backend_list = api.model('Backend list', {
+    'name': fields.String(required=True, description='Name of the backend'),
+    'backend': fields.Nested(backend, description='The backend'),
+})
+
+result = api.model('Result', {
+    'value': fields.String,
+    'cmd': fields.String
+})
 
 
-def build_cmd(backend_name, args):
-    if 'text' not in args.keys():
-        abort(204, message="Text to speak is invalid.")
+def abort_if_backend_isnt_available(name):
+    if name not in Backends:
+        api.abort(404, message="Backend {} isn't available.".format(name))
 
+
+def build_cmd(backend_name, text):
     default_parameters = Backends[backend_name]['parameters']
-    text_parameter = default_parameters['text']
-    text = args['text'].strip()
-    if 'p' not in text_parameter.keys():
-        text_cmd = [text]
-    else:
-        text_cmd = [text_parameter['p'], text]
+    text_arg = [p['arg'] for p in default_parameters if p['name'] == 'text'][0]
+    phoneme_arg = [p['arg'] for p in default_parameters if p['name'] == 'phoneme'][0]
+    quiet_arg = [p['arg'] for p in default_parameters if p['name'] == 'quiet'][0]
+
+    text_cmd = [text_arg, text.strip()]
 
     return [
         # Fill optional parameters
         Backends[backend_name]['binary'],
-        default_parameters['phoneme'],
-        default_parameters['quiet'],
+        phoneme_arg,
+        quiet_arg,
     ] + text_cmd
     # Text must appear at the end (to make espeak happy)
 
 
-parser = reqparse.RequestParser()
-parser.add_argument('text', type=str, help='Text to speak')
-parser.add_argument('parameters', type=str, help='Parameter to backend')
+parser = api.parser()
+parser.add_argument('text', type=str, help='Text to speak', required=True, location='form')
 
 
-# Backend
-# shows a single backend and lets you speak with it
+@ns.route('/<string:backend_name>')
+@api.doc(responses={404: 'Backend not found'},
+         params={'backend_name': 'The backend name'})
 class Backend(Resource):
+    '''Show a single backend and speak with it'''
+    @api.doc(description='Name should be one of {0}'.format(', '.join(Backends.keys())))
+    @api.marshal_with(backend)
     def get(self, backend_name):
+        '''Show a backend'''
         abort_if_backend_isnt_available(backend_name)
         return Backends[backend_name]
 
+    @api.doc(parser=parser)
+    @api.marshal_with(result, code=201)
     def post(self, backend_name):
+        '''Speak with a backend'''
         abort_if_backend_isnt_available(backend_name)
         args = parser.parse_args()
-        cmd = build_cmd(backend_name, args)
+        cmd = build_cmd(backend_name, args['text'])
         outputs = check_output(cmd).decode('utf-8')
-        phonemes = {'phonemes': outputs, 'cmd': cmd}
-        return phonemes, 200
+        return {'value': outputs, 'cmd': cmd}, 201
 
 
-# BackEndList
-# shows a list of all backends
+@ns.route('/')
 class BackendList(Resource):
+    '''Show a list of all available backends'''
+    @api.marshal_list_with(backend_list)
     def get(self):
-        return Backends
-
-
-#
-# Actually setup the Api resource routing here
-#
-api.add_resource(BackendList, '/backends')
-api.add_resource(Backend, '/backends/<backend_name>')
+        '''List all backends'''
+        return [{'name': name, 'backend': backend} for name, backend in Backends.items()]
 
 
 if __name__ == '__main__':
